e1fd3010f2bbeb67e5efdd1990a6d2a7ada5e7e7
nmwael <nino.martinez.wael@gmail.com>
2014-02-06 20:53:10 +0100

    Initial commit

---
+++ assignment/src/main/java/com/williamhill/App.java
@@ -0,0 +1,13 @@
+package com.williamhill;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ assignment/src/test/java/com/williamhill/BaseTests.java
@@ -0,0 +1,14 @@
+package com.williamhill;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BaseTests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
