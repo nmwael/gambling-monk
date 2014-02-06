b7aaed8105102ddced0a591b0ef2bbfe6885448a
nmwael <nino.martinez.wael@gmail.com>
2014-02-06 22:51:41 +0100

    commit

--- assignment/src/main/java/com/williamhill/App.java
+++ assignment/src/main/java/com/williamhill/App.java
@@ -1,13 +1,82 @@
 package com.williamhill;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Hello world!
- *
+ * 
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static final String NOT_EXISTING_DIRECTORY_HELP= "Not existing directory >";
+	public static final String NOT_A_VALID_DIRECTORY_ERROR = "Not a valid directory >";
+	public static final String NO_FILES_FOUND_HELP= "Sorry no files found";
+	public static final String CURRENT_VALID_ARGUMENTS_HELP = "-f \"pom.xml\" \"<directory>\"";
+	public static final String VALID_ARGUMENTS_HELP = "Please provide 3 arguments, valid arguments are:";
+
+	public static void main(String[] args) {
+		if (args.length != 3) {
+			System.out
+					.println(VALID_ARGUMENTS_HELP);
+			System.out.println(CURRENT_VALID_ARGUMENTS_HELP);
+		} else {
+			String fileFlagArgument = args[0];
+			String fileNameArgument = args[1];
+			String directoryArgument = args[2];
+
+			File directory = new File(directoryArgument);
+			if (directory.exists()) {
+				if (directory.isDirectory()) {
+
+					List<File> filesFound = findFiles(directory,
+							fileNameArgument);
+					
+					for (File file : filesFound) {
+						System.out.println(file.getAbsolutePath());
+
+					}
+					if(filesFound.isEmpty()){
+						System.out.println(NO_FILES_FOUND_HELP);
+
+					}
+				}
+				else{
+					System.out.println(NOT_A_VALID_DIRECTORY_ERROR
+							+ directory.getAbsolutePath() + "<");
+
+				}
+			} else {
+				System.out.println(NOT_EXISTING_DIRECTORY_HELP
+						+ directory.getAbsolutePath() + "<");
+
+			}
+		}
+
+	}
+
+	/**
+	 * grabbed from stackoverflow, modified for assignment
+	 * 
+	 * @param dir
+	 * @param fileName
+	 * @return
+	 */
+	private static List<File> findFiles(File dir, String fileName) {
+		ArrayList<File> result = new ArrayList<>(); // no need to store result
+													// as String, you're
+													// returning File anyway
+		File[] dirlist = dir.listFiles();
+
+		for (int i = 0; i < dirlist.length; i++) {
+			if (dirlist[i].isDirectory()) {
+				result.addAll(findFiles(dirlist[i], fileName));
+				if (!result.isEmpty())
+					break; // recursive call found the file; terminate the loop
+			} else if (dirlist[i].getName().matches(fileName)) {
+				result.add(dirlist[i]); // found the file; adding it
+			}
+		}
+		return result; // will return empty list if we didn't find anything
+	}
 }
--- assignment/src/test/java/com/williamhill/BaseTests.java
+++ assignment/src/test/java/com/williamhill/BaseTests.java
@@ -1,14 +1,132 @@
 package com.williamhill;
 
-import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.containsString;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
+import java.io.PrintStream;
+
+import org.jmock.Expectations;
+import org.jmock.Mockery;
+import org.jmock.lib.legacy.ClassImposteriser;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
-
 public class BaseTests {
+	
+	
+	private InputStream oldIn;
+	private PrintStream oldOut;
+	private PipedOutputStream pipe;
+	private PipedInputStream testIn;
+	private PrintStream mockOut;
+	private Mockery context;
+	
+	
+
+
+	@Before
+	public void setup() throws IOException
+	{
+		 context = new Mockery() {{
+		        setImposteriser(ClassImposteriser.INSTANCE);
+		    }};
+		    
+	    oldIn = System.in;
+	    oldOut = System.out;
+	    pipe = new PipedOutputStream();
+	    testIn = new PipedInputStream(pipe);
+	    mockOut = context.mock(PrintStream.class);
+	    System.setOut(mockOut);
+	    System.setIn(testIn);
+
+	}
+	
+
+
+	@After
+	public void resetAndCheckIfOk()
+	{
+		 context.assertIsSatisfied();
+	    System.setIn(oldIn);
+	    System.setOut(oldOut);
+	}
+
+
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test3ParametersWorking() {
+		
+	    Expectations exp = new Expectations()
+	    {
+	        {
+	        	
+	            oneOf(mockOut).println(with(org.hamcrest.text.StringContains.containsString("testFolders\\findme.txt")));
+	            oneOf(mockOut).println(with(org.hamcrest.text.StringContains.containsString("testFolders\\folderA\\findme.txt")));
+	            oneOf(mockOut).println(with(org.hamcrest.text.StringContains.containsString("testFolders\\folderA\\folderB\\findme.txt")));
+	            
+	            
+	        }
+	    };
+	    context.checking(exp);	
+	String[] arguments=new String[]{"-f","findme.txt","testFolders"}; 
+		App.main(arguments);
 	}
+	
+	@Test
+	public void test3ParametersBogusDirectory() {
+		
+	    Expectations exp = new Expectations()
+	    {
+	        {
+	        	
+	            oneOf(mockOut).println(with(org.hamcrest.text.StringContains.containsString(App.NOT_EXISTING_DIRECTORY_HELP)));
+	            
+	            
+	        }
+	    };
+	    context.checking(exp);	
+	String[] arguments=new String[]{"-f","findme.txt","testFalders"}; 
+		App.main(arguments);
+	}
+	@Test
+	public void test3ParametersDirectoryIsAFile() {
+		
+	    Expectations exp = new Expectations()
+	    {
+	        {
+	        	
+	            oneOf(mockOut).println(with(org.hamcrest.text.StringContains.containsString(App.NOT_A_VALID_DIRECTORY_ERROR)));
+	            
+	            
+	        }
+	    };
+	    context.checking(exp);	
+	String[] arguments=new String[]{"-f","findme.txt","testFolders\\findme.txt"}; 
+		App.main(arguments);
+	}
+
+	
+	
+	
+	@Test
+	public void test2Parameters() {
+	    Expectations exp = new Expectations()
+	    {
+	        {
+	        	
+	            oneOf(mockOut).println(with(org.hamcrest.text.StringContains.containsString(App.VALID_ARGUMENTS_HELP)));
+	            oneOf(mockOut).println(with(org.hamcrest.text.StringContains.containsString(App.CURRENT_VALID_ARGUMENTS_HELP)));	            
+	            
+	        }
+	    };
+	    context.checking(exp);
+		
+	String[] arguments=new String[]{"-f","pom.xml"}; 
+		App.main(arguments);
+	}
+
 
 }
